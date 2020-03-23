87b893ac19ea2b5007abf390080b3ba413849dd0
Dmitrii Tonkushin <d.tonkushin@smartcat.ai>
2020-03-23 15:19:50 +0500

    Add order model file, added reading from file

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,26 +1,29 @@
 /// <reference types="Cypress" />
 
 describe('Billing order test', function() {
-  beforeEach(function () {
-    cy.visit('/billing-order-form/')
-    cy.get("input[type='password']").type('Testing')
-    cy.contains("Submit").click()
+  before(function () {
+    cy.fixture('billing_orders.json').as('orders')
   })
 
-  it('Visits the billing order form', function() {
-    cy.get("input[name='wpforms[fields][0][first]']").type("John")
-    cy.get("input[name='wpforms[fields][0][last]']").type("Smith")
-    cy.get("input[name='wpforms[fields][1]']").type("fake-email@mail.com")
-    cy.get("input[name='wpforms[fields][2]']").type("8800200555")
-    cy.get("input[name='wpforms[fields][3][address1]']").type("Address 1")
-    cy.get("input[name='wpforms[fields][3][address2]']").type("Address 2")
-    cy.get("input[name='wpforms[fields][3][city]']").type("New-York")
-    cy.get("input[name='wpforms[fields][3][postal]']").type("54321")
-    cy.get("select[name='wpforms[fields][3][state]']").select("New York")
-    cy.get("textarea[name='wpforms[fields][6]']").type("Comment for billing order")
-    cy.get('[type="radio"]').first().check()
-    cy.get("button[name='wpforms[submit]']").click()
-
-    cy.get("div[id='wpforms-confirmation-24']").children("p").should('have.text', 'Thanks for contacting us! We will be in touch with you shortly.')
+  it('Send billing order with all required parameters', function() {
+    cy.get('@orders').each( (order) => {
+      cy.visit('/billing-order-form/')
+      cy.get("input[type='password']").type('Testing')
+      cy.contains("Submit").click()
+      cy.get("input[name='wpforms[fields][0][first]']").type(order.first_name)
+      cy.get("input[name='wpforms[fields][0][last]']").type(order.last_name)
+      cy.get("input[name='wpforms[fields][1]']").type(order.email)
+      cy.get("input[name='wpforms[fields][2]']").type(order.phone)
+      cy.get("input[name='wpforms[fields][3][address1]']").type(order.address_line1)
+      cy.get("input[name='wpforms[fields][3][address2]']").type(order.address_line2)
+      cy.get("input[name='wpforms[fields][3][city]']").type(order.city)
+      cy.get("input[name='wpforms[fields][3][postal]']").type(order.zip_code)
+      cy.get("select[name='wpforms[fields][3][state]']").select(order.state)
+      cy.get("textarea[name='wpforms[fields][6]']").type(order.comment)
+      cy.get('[type="radio"]').first().check()
+      cy.get("button[name='wpforms[submit]']").click()
+  
+      cy.get("div[id='wpforms-confirmation-24']").children("p").should('have.text', 'Thanks for contacting us! We will be in touch with you shortly.')
+    })
   })
 })
